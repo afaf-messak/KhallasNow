dadcd73343e1fbda2f149ec0d4069411b0aa112d
afaf-messak <messakafaf4@gmail.com>
2026-04-13 19:22:34 +0100

    supp

--- tmp_status_check.php
+++
@@ -1,9 +0,0 @@
-<?php
-require __DIR__ . '/vendor/autoload.php';
-$app = require __DIR__ . '/bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
-$kernel->bootstrap();
-$users = App\Models\User::select('status', \DB::raw('count(*) as total'))->groupBy('status')->get();
-foreach ($users as $u) {
-    echo $u->status . ': ' . $u->total . PHP_EOL;
-}
