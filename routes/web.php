d36eeb5125d8ad0d2d511731aae7202097e1fec4
SalahEddine752 <salaheddineben777@gmail.com>
2026-04-30 21:36:52 +0100

    fix: resolve merge conflict in web.php - keep all routes

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,54 @@
 <?php
 
 use App\Http\Controllers\AdminController;
-<<<<<<< HEAD
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ContractController;
 use App\Http\Controllers\InvoiceController;
 use App\Http\Controllers\PaymentController;
 use Illuminate\Support\Facades\Route;
 
-=======
-use App\Http\Controllers\AuthController;
-use Illuminate\Support\Facades\Route;
-
-
-
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
 */
 
+// ==================== HOME ====================
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
 
+// ==================== CLIENT AUTH ====================
+Route::prefix('client')->group(function () {
+    Route::get('/login', [AuthController::class, 'showClientLogin'])->name('client.login');
+    Route::post('/login', [AuthController::class, 'loginClient'])->name('client.login.submit');
+    Route::get('/dashboard', [AuthController::class, 'dashboard'])->middleware('auth')->name('client.dashboard');
+    Route::post('/logout', [AuthController::class, 'logoutClient'])->middleware('auth')->name('client.logout');
+});
+
+// ==================== CONTRATS ====================
+Route::middleware(['auth'])->group(function () {
+    Route::get('/contrats', [ContractController::class, 'index'])->name('contrats.index');
+    Route::get('/contrats/create', [ContractController::class, 'create'])->name('contrats.create');
+    Route::post('/contrats', [ContractController::class, 'store'])->name('contrats.store');
+    Route::get('/contrats/{contrat}', [ContractController::class, 'show'])->name('contrats.show');
+    Route::get('/contrats/{contrat}/edit', [ContractController::class, 'edit'])->name('contrats.edit');
+    Route::put('/contrats/{contrat}', [ContractController::class, 'update'])->name('contrats.update');
+    Route::delete('/contrats/{contrat}', [ContractController::class, 'destroy'])->name('contrats.destroy');
+    Route::get('/contrats/{contrat}/optimisation', [ContractController::class, 'optimisation'])->name('contrats.optimisation');
+});
+
+// ==================== FACTURES ====================
+Route::middleware(['auth'])->group(function () {
+    Route::get('/factures', [InvoiceController::class, 'index'])->name('factures.index');
+});
+
+// ==================== PAIEMENTS ====================
+Route::middleware(['auth'])->group(function () {
+    Route::get('/paiements', [PaymentController::class, 'index'])->name('paiements.index');
+});
+
+// ==================== ADMIN AUTH ====================
 Route::prefix('admin')->group(function () {
     Route::get('/login', [AdminController::class, 'showLoginForm'])->name('admin.login');
     Route::post('/login', [AdminController::class, 'login'])->name('admin.login.submit');
@@ -51,56 +74,8 @@ Route::prefix('admin')->group(function () {
         Route::get('/settings', [AdminController::class, 'settings'])->name('admin.settings');
     });
 });
->>>>>>> c9737e2a47c06691e37619a0fbc6012f34ccda63
-Route::get('/', function () {
-    return view('welcom');
-})->name('home');
 
-// ==================== CONTRATS ====================
-Route::middleware(['auth'])->group(function () {
-    Route::get('/contrats', [ContractController::class, 'index'])->name('contrats.index');
-    Route::get('/contrats/create', [ContractController::class, 'create'])->name('contrats.create');
-    Route::post('/contrats', [ContractController::class, 'store'])->name('contrats.store');
-    Route::get('/contrats/{contrat}', [ContractController::class, 'show'])->name('contrats.show');
-    Route::get('/contrats/{contrat}/edit', [ContractController::class, 'edit'])->name('contrats.edit');
-    Route::put('/contrats/{contrat}', [ContractController::class, 'update'])->name('contrats.update');
-    Route::delete('/contrats/{contrat}', [ContractController::class, 'destroy'])->name('contrats.destroy');
-    Route::get('/contrats/{contrat}/optimisation', [ContractController::class, 'optimisation'])->name('contrats.optimisation');
-});
-
-<<<<<<< HEAD
-// ==================== FACTURES (Liste + Filtres) ====================
-Route::middleware(['auth'])->group(function () {
-    Route::get('/factures', [InvoiceController::class, 'index'])->name('factures.index');
-});
-
-// ==================== PAIEMENTS (Historique) ====================
-Route::middleware(['auth'])->group(function () {
-    Route::get('/paiements', [PaymentController::class, 'index'])->name('paiements.index');
-});
-
-// ==================== ADMIN ====================
-Route::middleware(['auth', 'admin'])->prefix('admin')->name('admin.')->group(function () {
-    Route::get('/users', [AdminController::class, 'users'])->name('users.index');
-    Route::get('/users/create', [AdminController::class, 'create'])->name('users.create');
-    Route::post('/users', [AdminController::class, 'store'])->name('users.store');
-    Route::get('/users/{user}', [AdminController::class, 'show'])->name('users.show');
-    Route::get('/users/{user}/edit', [AdminController::class, 'edit'])->name('users.edit');
-    Route::put('/users/{user}', [AdminController::class, 'update'])->name('users.update');
-    Route::delete('/users/{user}', [AdminController::class, 'destroy'])->name('users.destroy');
-    Route::post('/users/{user}/status', [AdminController::class, 'toggleStatus'])->name('users.status');
-});
-=======
-Route::prefix('client')->group(function () {
-    Route::get('/login', [AuthController::class, 'showClientLogin'])->name('client.login');
-    Route::post('/login', [AuthController::class, 'loginClient'])->name('client.login.submit');
-    Route::get('/dashboard', [AuthController::class, 'dashboard'])->middleware('auth')->name('client.dashboard');
-    Route::post('/logout', [AuthController::class, 'logoutClient'])->middleware('auth')->name('client.logout');
-});
-
-// Note: admin routes are defined in the `Route::prefix('admin')` group above.
-// Keep a root admin entry if you want `/admin` to show the dashboard.
+// Redirect /admin vers dashboard
 Route::get('/admin', function () {
-    return redirect()->route('admin.users.index');
+    return redirect()->route('admin.dashboard');
 })->name('admin.root');
->>>>>>> c9737e2a47c06691e37619a0fbc6012f34ccda63
